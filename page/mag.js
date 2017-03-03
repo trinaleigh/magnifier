b284754c49a501bd5e0a3b29d5c55872ae41211c
trinaleigh <trinaleigh@users.noreply.github.com>
2017-03-03 10:29:03 -0500

    configurable

--- page/mag.js
+++ page/mag.js
@@ -1,12 +1,28 @@
 window.onload = function () {
 
+	// get dimensions
 	const contentArea = document.body;
-	const image = document.querySelector('img');
+	const image = document.querySelector('img'); // image to be magnified
 	const glass = document.getElementById('glass');
+	const factor = document.getElementById('factor');
 
-	const magWidth = 1600;
-	const magHeight = 3200;
+	var magnifier = factor.value; // set factor for magnification
+	var magWidth = image.offsetWidth*magnifier;
+	var magHeight = image.offsetHeight*magnifier;
 
+	glass.style.backgroundSize = `${magWidth}px ${magHeight}px`
+
+	// listeners
+	image.addEventListener("mouseenter", enter);
+	contentArea.addEventListener("mousemove", magnify);
+	factor.addEventListener("change", changeGlass);
+
+	// show magnifier
+	function enter(event){
+		glass.classList.add("on-page");
+	}
+
+	// magnify content within the circle
 	function magnify(event){
 
 		var coords = image.getBoundingClientRect();
@@ -16,19 +32,26 @@ window.onload = function () {
 
 		var xPercent = (event.pageX - window.scrollX - coords.left) / image.offsetWidth * magWidth;
 		var yPercent = (event.pageY - window.scrollY - coords.top) / image.offsetHeight * magHeight;
-		glass.style.backgroundPosition = `${-xPercent + glass.offsetWidth/2 - 5}px ${-yPercent + glass.offsetHeight/2 - 5}px`;
+		glass.style.backgroundPosition = 
+			`${-xPercent + glass.offsetWidth/2 - glass.clientLeft}px ${-yPercent + glass.offsetHeight/2 - glass.clientLeft}px`;
+
+		var glassPos = glass.getBoundingClientRect();
+
+		// if mouse is outside the image area, don't show magnifier
+		if (glassPos.right - glass.offsetWidth/2 + glass.clientLeft <= coords.left || 
+			glassPos.left + glass.offsetWidth/2 - glass.clientLeft >= coords.right ||
+			glassPos.top + glass.offsetWidth/2 - glass.clientLeft >= coords.bottom ||
+			glassPos.bottom - glass.offsetWidth/2 + glass.clientLeft <= coords.top ){
+			glass.classList.remove("on-page");
+		}
 	}
 
-	function enter(event){
-		glass.classList.add("on-page");
+	function changeGlass(event){
+		magnifier = this.value;
+		magWidth = image.offsetWidth*magnifier;
+		magHeight = image.offsetHeight*magnifier;
+		glass.style.backgroundSize = `${magWidth}px ${magHeight}px`
 	}
 
-	function exit(event){
-		glass.classList.remove("on-page")
-	}
-
-
-	contentArea.addEventListener("mousemove", magnify);
-	contentArea.addEventListener("mouseenter", enter);
 }
 
