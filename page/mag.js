789a1a7cd35b4922c664b2f1973f22cc1a5543d3
trinaleigh <trinaleigh@users.noreply.github.com>
2017-03-02 23:07:23 -0500

    fix position

--- page/mag.js
+++ page/mag.js
@@ -1,21 +1,34 @@
-const contentArea = document.body;
-const glass = document.getElementById('glass');
+window.onload = function () {
 
-function magnify(event){
-	console.log("moved");
-	glass.style.top = event.pageY - glass.offsetHeight/2;
-	glass.style.left = event.pageX - glass.offsetWidth/2;
+	const contentArea = document.body;
+	const image = document.querySelector('img');
+	const glass = document.getElementById('glass');
+
+	const magWidth = 1600;
+	const magHeight = 3200;
+
+	function magnify(event){
+
+		var coords = image.getBoundingClientRect();
+
+		glass.style.left = event.pageX - glass.offsetWidth/2;
+		glass.style.top = event.pageY - glass.offsetHeight/2;
+
+		var xPercent = (event.pageX - window.scrollX - coords.left) / image.offsetWidth * magWidth;
+		var yPercent = (event.pageY - window.scrollY - coords.top) / image.offsetHeight * magHeight;
+		glass.style.backgroundPosition = `${-xPercent + glass.offsetWidth/2 - 5}px ${-yPercent + glass.offsetHeight/2 - 5}px`;
+	}
+
+	function enter(event){
+		glass.classList.add("on-page");
+	}
+
+	function exit(event){
+		glass.classList.remove("on-page")
+	}
+
+
+	contentArea.addEventListener("mousemove", magnify);
+	contentArea.addEventListener("mouseenter", enter);
 }
 
-function enter(event){
-	glass.classList.add("on-page");
-}
-
-function exit(event){
-	console.log("exited area");
-	glass.classList.remove("on-page")
-}
-
-
-contentArea.addEventListener("mousemove", magnify);
-contentArea.addEventListener("mouseenter", enter);
